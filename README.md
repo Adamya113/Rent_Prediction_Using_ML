da61a9cfada8b7de1eaf6e6f38b0753bda310055
Adamya Gaur <gauradamya725@gmail.com>
2025-01-16 15:38:30 +0530

    Data Validation Added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Rent_Prediction_Using_ML
-A rent prediction webapp, based on Delhi's dataset. Includes complete CI/CD pipeline along woth MlFlow and AWS ! 
+A rent prediction webapp, based on Delhi's dataset. Includes complete CI/CD pipeline along with MlFlow and AWS ! 
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import DataIngestionConfig
+from mlProject.entity.config_entity import (DataIngestionConfig,
+                                            DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -15,6 +16,7 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
     
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
         create_directories([config.root_dir])
@@ -25,5 +27,19 @@ class ConfigurationManager:
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir 
         )
-
         return data_ingestion_config
+
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
